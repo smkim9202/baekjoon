e4a9ee7e8999c49d1d1fdc21afe1933d6902a045
개발슝이 <smkim9202@gmail.com>
2021-01-26 21:11:10 +0900

    Create 2588.py
    곱셈

---
+++ 2588.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+
+a = int(a)
+b1 = int(b[2])
+b2 = int(b[1])
+b3 = int(b[0])
+
+A = a * b1
+B = a * b2
+C = a * b3
+
+print(A)
+print(B)
+print(C)
+print(A+(B*10)+(C*100))
