43419bf87e38fc42881f2a4124b37deeedc1779f
개발슝이 <smkim9202@gmail.com>
2021-02-12 00:18:55 +0900

    Update 5622.py
    다이얼

--- 5622.py
+++ 5622.py
@@ -1,6 +1,7 @@
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 num = [2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9]
 
+
 words = input()
 result = 0
 for x in range(len(words)):
