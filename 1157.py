adbca13322390659092267eb62e8d1bbcdc8dffe
개발슝이 <smkim9202@gmail.com>
2021-02-15 22:45:33 +0900

    Create 1157.py
    단어 공부

---
+++ 1157.py
@@ -0,0 +1,10 @@
+cnt_li = []
+for i in alphabet:
+    cnt = words.count(i)
+    cnt_li.append(cnt)
+
+if cnt_li.count(max(cnt_li)) > 1:
+    print('?')
+else:
+    result = alphabet[cnt_li.index(max(cnt_li))]
+    print(result)
