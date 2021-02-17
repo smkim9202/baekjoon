6ef393c63a40be78e254f070d2b46315fb4fb7aa
개발슝이 <smkim9202@gmail.com>
2021-02-17 18:09:27 +0900

    Update 1316.py

--- 1316.py
+++ 1316.py
@@ -1,16 +1,17 @@
 N = int(input())
-alphabetcnt = 0
 
+cnt = 0
 for i in range(N):
     words = input()
-    for x in range(len(words)-1):
-        if words[x] == words[x + 1]:
-            words.replace(words[x], '*')
-            for y in words:
-                if y != '*':
-                    if words.count(y) != 1:
-                        alphabetcnt += 1
-    if alphabetcnt > 0:
-        cnt = N -1       
+    for x in range(len(words)): 
+        if x == len(words)-1:
+            pass
+        else:
+            if words[x] == words[x + 1]:
+                words.replace(words[x], '*') 
+            else:
+                if words[x] in words[x+1:]:
+                    cnt += 1
+                    break
 
-print(cnt)
+print(N - cnt)
