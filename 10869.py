9056228d090165ff03ab5144063d2530ca6359b7
개발슝이 <smkim9202@gmail.com>
2021-04-02 01:27:59 +0900

    Update 10869.py

--- 10869.py
+++ 10869.py
@@ -1,9 +1,3 @@
-A, B = input().split()
-A = int(A)
-B = int(B)
+A, B = map(int,(input().split()))
 
-print(A+B)
-print(A-B)
-print(A*B)
-print(A//B)
-print(A%B)
+print(A+B, A-B, A*B, A//B, A%B, sep='\n')
