c35f4451b1186c051d0aea792a081eabcc44629b
개발슝이 <smkim9202@gmail.com>
2021-04-05 19:48:49 +0900

    Update 9498.py

--- 9498.py
+++ 9498.py
@@ -1,15 +1,12 @@
-result = input()
+score = int(input())
 
-result = int(result)
-
-
-if 90 <= result <=100:
+if 90 <= score <=100:
     print('A')
-elif 80 <= result <=89:
+elif 80 <= score <=89:
     print('B')
-elif 70 <= result <=79:
+elif 70 <= score <=79:
     print('C')
-elif 60 <= result <=69:
+elif 60 <= score <=69:
     print('D')
 else:
     print('F')
