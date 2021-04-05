e133b2e0631cd2529955142fb8d66db865d926b0
개발슝이 <smkim9202@gmail.com>
2021-04-05 20:15:24 +0900

    Update 2753.py

--- 2753.py
+++ 2753.py
@@ -1,15 +1,12 @@
-leap_year = input()
+leapyear = int(input())
 
-leap_year = int(leap_year)
-
-
-if leap_year % 4 == 0:
-    if leap_year % 100 == 0:
-        if leap_year % 400 == 0:
+if leapyear % 4 == 0:
+    if leapyear % 100 != 0:
+        print(1)
+    else:
+        if leapyear % 400 == 0:
             print(1)
         else:
             print(0)
-    else:
-        print(1)
 else:
     print(0)
